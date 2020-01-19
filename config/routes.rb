3ea9d106bfe7b4a277d0aee715ab1caa5f806639
Taiga Miwa <taiga.miwa888@gmail.com>
2020-01-19 10:20:07 +0900

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resouces :bank_accounts, only: %i(index new create edit update)
+  resources :bank_accounts, only: %i(index new create edit update)
   root to: 'bank_accounts#index'
 end
